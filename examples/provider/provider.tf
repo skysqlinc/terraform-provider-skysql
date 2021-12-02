80a1c8f29ae9aebb22bcd5961edf2c2968045229
Don Mayo <don.mayo@mariadb.com>
2021-12-01 23:30:52 -0500

    Include `required_providers` in provider doc
    We'll need this so long as we're not in the terraform registry, so I
    figure let's have it in the docs for now and we can remove it later if
    it becomes unnecessary. This will also help anyone wanting to set up for
    the first time and are looking at the docs (rather than directly at the
    examples).

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,1 +1,10 @@
+terraform {
+  required_providers {
+    skysql = {
+      source  = "registry.terraform.io/mariadb-corporation/skysql"
+      version = "0.0.1"
+    }
+  }
+}
+
 provider "skysql" {}
